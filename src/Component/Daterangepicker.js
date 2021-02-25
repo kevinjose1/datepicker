558c5d45b3c7eed44c7644cbdf3e704c4394922e
kevinjose1 <kjose08@gmail.com>
2021-02-25 23:05:34 +0530

    25-02-2021

--- src/Component/Daterangepicker.js
+++ src/Component/Daterangepicker.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
+import Charts from '../Component/chart/Charts';
 import "react-datepicker/dist/react-datepicker.css";
 import './Datepicker.css';
 
 // CSS Modules, react-datepicker-cssmodules.css
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
-
+const todayDate= new Date();
+console.log(todayDate)
 const Daterange = () => {
  const [date, newDate ] = useState({
    currentDate: '',
@@ -37,18 +39,28 @@ const returnCondition = () => {
    }
   
 }
+
+
    return (
       <div > 
       <div className="form">
 
      
-  <DatePicker  selected={date.currentDate} placeholderText="Start Date" onChange = {date => changeDate(date)} className="datpicker-input"/>
-  <DatePicker minDate = {date.maxDate} placeholderText="End Date" selected = {date.maxDate} onChange = {date => changeMaxDate(date)} className="datpicker-input"/>
+  <DatePicker showYearDropdown
+      dateFormatCalendar="MMMM"
+      minDate={new Date()}
+      scrollableYearDropdown selected={date.currentDate} placeholderText="Start Date" onChange = {date => changeDate(date)} className="datpicker-input"/>
+  <DatePicker showYearDropdown dateFormatCalendar="MMMM"
+    
+    scrollableYearDropdown  minDate = {date.maxDate} placeholderText="End Date" selected = {date.maxDate} onChange = {date => changeMaxDate(date)} className="datpicker-input"/>
   <button type="submit" className="btn btn-primary" disabled = {returnCondition()} className="submit-btn">Apply</button>
       </div>
+     
+      <Charts></Charts>
       </div>
   
   );
 };
+
 
 export default Daterange;
---
+++ src/Component/chart/Charts.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Line , Bar , Pie} from 'react-chartjs-2';// Make  Chart
+
+function Chart(){
+    const linedata = {
+        labels:[ 'Jan','Feb','March','April','May'],
+        datasets:[
+            {
+                label:'Graph - 1',
+                data : [3, 2, 2, 1, 5],
+                borderColor:['rgba(255,0,0,0.3)'],
+                backgroundColor:['rgba(0,0,255,0.3)']
+
+            },
+            {
+                label:'Graph - 2',
+                data : [5,8,4,6,3],
+                borderColor:['rgba(255,255,0,0.3)'],
+                backgroundColor:['rgba(0,255,0,0.3)']
+            }
+        ]
+    }
+
+    const piedata = {
+        labels:[ 'Jan','Feb','March','April','May'],
+        datasets:[
+            {
+                label:'Graph - 1',
+                data : [3, 2, 2, 1, 5],
+               
+                backgroundColor:['rgb(153, 153, 255)','rgb(255, 0, 102)','rgb(102, 153, 0)','rgb(153, 0, 0)','rgba(0,0,255,0.3)']
+            },
+        ]
+    }
+    return (
+        <div className="chart">
+            <p>Line Chart</p>
+            <hr></hr>
+            <Line data={linedata} />
+            <p>Bar Chart</p>
+            <hr></hr>
+            <Bar data={linedata} />
+            <p>Pie Chart</p>
+            <hr></hr>
+            <Pie data={piedata} />
+        </div>
+    )
+}
+
+export default Chart;
